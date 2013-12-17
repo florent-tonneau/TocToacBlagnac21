dc84a969cd726ca6d79137fd4ac96456c613f1f0
florent-tonneau <florent.tonneau@etu.univ-tlse2.fr>
2013-12-17 09:04:47 +0100

    Codage Modèle Story 1
    Membre pouvant accéder aux événements et aux inscrits de celui-ci

--- src/Modele/Event.java
+++ src/Modele/Event.java
@@ -4,10 +4,50 @@ import java.util.Vector;
 
 public class Event {
 	private String date;
+	private String horaire;
 	private String titre;
 	private Vector<Membre> participants;
 	
-	public Event(String titre, String date){
+	public Event(String _titre, String _date, String _horaire, Vector <Membre> _participants){
+		this.date=_date;
+		this.horaire=_horaire;
+		this.titre=_titre;
 		
+		for (int i=0;i<_participants.size();i++)
+			participants.add(_participants.elementAt(i));
 	}
+	
+	public Vector<Membre> getParticipants() {
+		return this.participants;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getHoraire() {
+		return horaire;
+	}
+
+	public void setHoraire(String horaire) {
+		this.horaire = horaire;
+	}
+
+	public String getTitre() {
+		return titre;
+	}
+
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+	public void setParticipants(Membre participant) {
+		this.participants.add(participant);
+	}
+	
+	
 }
--- src/Modele/Membre.java
+++ src/Modele/Membre.java
@@ -4,4 +4,29 @@ public class Membre {
 	private int idMembre;
 	private String nom;
 	
+	
+	public Membre (int _id, String _nom) {
+		this.idMembre=_id;
+		this.nom=_nom;
+	}
+
+
+	public int getIdMembre() {
+		return idMembre;
+	}
+
+
+	public void setIdMembre(int idMembre) {
+		this.idMembre = idMembre;
+	}
+
+
+	public String getNom() {
+		return nom;
+	}
+
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
 }
