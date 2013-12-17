0b2b207ccac4401afbd4b2f791e287f202b0242e
mailinou <virginie.mailan.nguyen@gmail.com>
2013-12-17 09:28:29 +0100

    aaa

---
+++ src/Controleur/CtrlListeAct.java
@@ -0,0 +1,27 @@
+package Controleur;
+
+import java.awt.Event;
+import java.util.Vector;
+
+public class CtrlListeAct {
+
+	private Vector<Event> listeA;
+	
+	public CtrlListeAct()
+	{
+		this.listeA = new Vector<Event>();
+		
+	}
+	
+	public void addAct( Event e)
+	{
+		this.listeA.add(e);
+	}
+	
+	public void removeAct( Event e)
+	{
+		this.listeA.remove(e);
+	}
+
+
+}
