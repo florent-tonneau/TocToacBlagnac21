68fd6b22186f6971eb29cad6fa26b957c7e47847
mailinou <virginie.mailan.nguyen@gmail.com>
2014-01-06 10:19:54 +0100

    Doc CtrlListeAct
    Vincent doit remettre ses 3-4 dernière lignes

--- src/Controleur/CtrlListeAct.java
+++ src/Controleur/CtrlListeAct.java
@@ -24,32 +24,80 @@ public class CtrlListeAct {
 		this.addAct(a1);		
 	}
 	
+	/**
+	 *Nom: demarrerControleur
+	 *Description: Démarre le contrôleur pour créer et ouvrir une fenêtre
+	 *Entrée: -
+	 *Sortie: -
+	 *Retour: -
+	 *Précondition: -
+	 * */
 	public void demarrerControleur(){
 		this.vm = new VueMembre("Vue membre", this);
 		this.vm.setVisible(true);
 		this.vm.majVueMembre();
 	}
 	
+	/**
+	 *Nom: addAct
+	 *Description: permet d'ajouter une activité passée en paramètre à la liste des activités
+	 *Entrée: Activite e
+	 *Sortie: listeA mise à jour
+	 *Retour: -
+	 *Précondition: -
+	 * */
 	public void addAct( Activite e)
 	{
 		this.listeA.addElement(e);
 	}
 	
+	/**
+	 *Nom: removeAct
+	 *Description: permet de supprimer une activité passée en paramètre de la liste d'activité
+	 *Entrée: Activite e
+	 *Sortie: listeA mise à jour
+	 *Retour: -
+	 *Précondition: l'activité e doit exister
+	 * */
 	public void removeAct( Activite e)
 	{
 		this.listeA.remove(e);
 	}
 
+	/**
+	 *Nom: getNbActivites
+	 *Description: Retourne le nombre total d'activités dans la liste
+	 *Entrée: -
+	 *Sortie: -
+	 *Retour: la taille de la liste
+	 *Précondition: liste non vide
+	 * */
 	public int getNbActivites()
 	{
 		return this.listeA.size();
 	}
 	
+	/**
+	 *Nom: getListeActivite 
+	 *Description: Retourne un vecteur (tableau) d'activités
+	 *Entrée: -
+	 *Sortie: -
+	 *Retour: une liste d'activités
+	 *Précondition: -
+	 * */
 	public Vector<Activite> getListeActivite()
 	{
 		return this.listeA;
 	}
 	
+	/**
+	 *Nom: getMembreActivite
+	 *Description: Retourne une liste de membres inscrit à une activité passée en paramètre
+	 *Entrée: Activite _act
+	 *Sortie: -
+	 *Retour: une liste de membre
+	 *Précondition: l'activité doit exister
+	 * */
 	public Vector<Membre> getMembreActivite (Activite _act) {
 			
 		for (int i=0; i<this.getNbActivites();i++) {
@@ -59,6 +107,14 @@ public class CtrlListeAct {
 		return null;		
 	}
 	
+	/**
+	 *Nom: ctrlStopVueMembre
+	 *Description: Ferme la vue membre
+	 *Entrée: -
+	 *Sortie: -
+	 *Retour: -
+	 *Précondition: la vue doit être active
+	 * */
 	public void ctrlStopVueMembre() {
 		this.vm.setVisible(false);
 	}
